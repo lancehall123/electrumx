afa320897158939f16cdb1add9ea25a79b9a84b3
NeblioTeam <neblioteam@gmail.com>
2018-03-26 14:22:06 -0400

    Test NTP1 blocking

--- server/controller.py
+++ server/controller.py
@@ -815,6 +815,11 @@ class Controller(ServerBase):
         with urllib.request.urlopen("https://ntp1node.nebl.io:8080/v3/addressinfo/"+address) as url:
             data = json.loads(url.read().decode())
             self.logger.info(data)
+        for utxo in data.utxos:
+        	if len(utxo.tokens) > 0:
+        		self.logger.error('ERROR: NTP1 Tokens found at address '+address)
+        		return []
+        self.logger.info('No NTP1 Tokens found at address '+address)
         hashX = self.address_to_hashX(address)
         return await self.hashX_listunspent(hashX)
 
